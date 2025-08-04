db127ac39c9cf71454d9e6a7fc01e41cb50d1978
Tausif0209 <tk027592@gmail.com>
2025-08-04 23:27:20 +0530

    Leetcode 1. Two Sum

---
+++ TwoSum.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class TwoSum {
+   public int[] twoSum(int[] nums, int target) {
+      if(nums.length==2) return new int[]{0,1};
+      HashMap<Integer,Integer> map=new HashMap<>();
+      for(int i=0;i<nums.length;i++){
+        if(map.containsKey(target-nums[i])) return new int[]{map.get(target-nums[i]),i};
+        map.put(nums[i],i);
+      } return new int[]{-1,-1};
+    }
+}
